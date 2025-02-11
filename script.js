84b43ac19163af6512a2da4133fdcdcca6bfcb4d
realBruno <soubruno@protonmail.com>
2025-02-11 14:53:17 -0300

    Add dark mode

--- script.js
+++ script.js
@@ -2,12 +2,28 @@
 const milliseconds = document.getElementById("milliseconds");
 milliseconds.style.display = "none";
 
+function reset() {
+    // esconde milisegundos
+    milliseconds.style.display = "none";
+}
+
 function start() {
     // exibe milisegundos
     milliseconds.style.display = "initial";
 }
 
-function reset() {
-    // esconde milisegundos
-    milliseconds.style.display = "none";
+/* ************ DARK & LIGHT MODE ************ */
+
+function dark_mode()
+{
+    document.body.style.background = "black";
+    document.getElementById("mode").innerHTML = 
+        `<button id="light" onclick="light_mode()">Light</button>`;
+}
+
+function light_mode()
+{
+    document.body.style.background = "white";
+    document.getElementById("mode").innerHTML = 
+        `<button id="dark" onclick="dark_mode()">Dark</button>`;
 }
